5bd9350dfe110810be22c80bb886896a57a65b02
Iyris Vigil <iyrisvigil@gmail.com>
2020-09-09 01:42:49 +0000

    implemented character counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+//responsible only for character counter
+$(document).ready(function() {
+  // --- our code goes here ---
+  const $tweet = $('#tweet-text')
+
+  $tweet.on('keyup', function event() { //this points to the textarea #tweet-text
+    console.log('this', this)
+    const limit = 140
+    const length = $(this).val().length;
+    // console.log(length)
+    const remainingChars = limit - length;
+    // console.log("left:", remainingChars)
+    // const counter = $(this).parent(["form"]).children(".counter-parent").children(".counter");
+    const counter = $(this).siblings(".counter");
+    console.log(counter)
+    $('#tweetcounter').text(remainingChars);
+    if (length > 140) {
+      $('#tweetcounter').css('color','red');
+    } 
+  
+  })
+});
