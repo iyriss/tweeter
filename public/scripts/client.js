475e788ceac0f68cc42b362b3e22d51c1a8dcc70
Iyris Vigil <iyrisvigil@gmail.com>
2020-09-10 02:30:13 +0000

    implemented form submission using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,45 +5,92 @@
  */
 
  //Take in a tweet object that will be responsbile for returning a tweet <article>
-const createTweetElement = function (data) {
-return `
-  <article class="tweet-example">
-    <header id = "tweet-header">
-      <div id="posts">
-        <img src="${data.user.avatars}">
-        <p>${data.user.name}</p>
-      </div>
-      <span>${data.user.handle}</span>
-    </header>
-    <p>${data.content.text}</p>
-    <footer>
-      <p>${data.created_at}</p>
-      <span>
-        <i class="fas fa-flag"></i> 
-        <i class="far fa-heart"></i>
-        <i class="fas fa-retweet"></i>
-      </span>
-    </footer>
-  </article>
-`
-
-}
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data =  [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const createTweetElement = function (tweetData) {
+  let $tweet = '';
+
+  let user = tweetData.user.name;
+  let avatar = tweetData.user.avatars;
+  let handle = tweetData.user.handle;
+  let content = tweetData.content.text;
+  let createdAt = tweetData.createdAt;
+
+  $tweet =
+    `
+    <article class="tweet-example">
+      <header id = "tweet-header">
+        <div id="posts">
+          <img src="${avatar}">
+          <p>${user}</p>
+        </div>
+        <span>${handle}</span>
+      </header>
+      <p>${content}</p>
+      <footer>
+        <p>${createdAt}</p>
+        <span>
+          <i class="fas fa-flag"></i> 
+          <i class="far fa-heart"></i>
+          <i class="fas fa-retweet"></i>
+        </span>
+      </footer>
+    </article>
+    `;
+  return $tweet;
 }
+
+const renderTweets = function(tweets) {
+  // loops through tweets // calls createTweetElement for each tweet
+  tweets.forEach(function(element) {
+    let $tweet = createTweetElement(element);
+    // takes return value and appends it to the tweets container
+    $("#tweets-container").append($tweet);
+  })
+}
+
+// const $("<section>")
+
+
 $(document).ready(function (){
-  const $tweet = createTweetElement(tweetData);
+  $("#new-tweet").on("submit", function (event) {
+    event.preventDefault();
+    // console.log( $(this).serialize() );
+    // console.log("test")
+    // console.log(event)
+    $.ajax('/tweets', {method: 'POST', data: $("#new-tweet").serialize()})
+    .then(function (response) {
+    console.log('Success: ', response);
+    // console.log(this)
+    });
+  })
+  // const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data)
 });
